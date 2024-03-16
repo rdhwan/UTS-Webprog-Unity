024f556463766b496c3561258c703017f49354bd
Exosuit <intltcsfleader@gmail.com>
2024-03-16 14:41:36 +0700

    feat: image folder

--- src/Public/index.php
+++ src/Public/index.php
@@ -1,3 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
 
-// Login - exosuit
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/output.css">
+    <link rel="shortcut icon" href="./images/favicon.png" type="image/x-icon">
+    <title>UnityBook</title>
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
+</head>
+
+<body class="flex flex-col min-h-screen min-w-full bg-[url('./images/bg.png')] bg-cover font-inter">
+
+
+
+    <footer
+        class="flex items-center justify-center text-white font-semibold bg-[url('../images/bottom.svg')] bg-cover bg-top">
+        <p class="text-center z-10 p-4">©2024 UnityBook. All rights reserved.</p>
+    </footer>
+
+</body>
+
+</html>
