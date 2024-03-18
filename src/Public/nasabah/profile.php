d436d38a4fa030ffa88d89896d55e88e37a1fc9f
AlbertTech23 <alberttirtokusumo@gmail.com>
2024-03-18 14:14:00 +0700

    init: profile page layouting terkendala tailwind

---
+++ src/Public/nasabah/profile.php
@@ -0,0 +1,160 @@
+<?php
+
+require_once __DIR__ . "/../../Middleware/checkNasabah.php";
+
+
+if ($user->role !== "nasabah") {
+    $_SESSION["error"] = "You are not authorized to access this page";
+
+    header("Location: /src/Public/admin/index.php");
+    exit;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/output.css">
+    <link rel="shortcut icon" href="../images/logo.png" type="image/x-icon">
+    <title>Unity</title>
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
+</head>
+
+<body class="flex flex-col min-h-screen min-w-full font-inter p-4 md:p-8">
+    <!-- navbar -->
+    <div class="flex flex-row items-center justify-between gap-8">
+
+        <details class="visible md:hidden dropdown">
+            <summary class="btn btn-ghost text-[#E178C5]">
+                <i class="ph ph-list text-4xl"></i>
+            </summary>
+            <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                <li>
+                    <a href="./history.php" class="btn btn-ghost flex items-center justify-start font-semibold text-lg text-[#E178C5]">History
+                    </a>
+                </li>
+                <li>
+                    <details class="dropdown">
+                        <summary class="btn btn-ghost flex items-center justify-start font-semibold text-lg text-[#E178C5]">
+                            <p>Payment</p>
+                        </summary>
+                        <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                            <li>
+                                <a href="/src/Public/nasabah/wajib.php" class="text-[#E178C5] font-semibold">
+                                    <i class="ph ph-wallet text-xl"></i>
+                                    Tabungan Wajib
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/src/Public/nasabah/sukarela.php" class="text-[#FFB38E] font-semibold">
+                                    <i class="ph ph-hand-coins text-xl"></i>
+                                    Tabungan Sukarela
+                                </a>
+                            </li>
+                        </ul>
+                    </details>
+                </li>
+            </ul>
+        </details>
+
+
+
+        <div class="hidden md:flex flex-row items-center gap-8">
+            <a href="/src/Public/nasabah/index.php">
+                <img src="../images/logo.png" class="w-36 md:w-24" />
+            </a>
+
+            <a href="./history.php" class="btn btn-ghost text-center font-semibold text-lg text-[#E178C5]">History</a>
+            <details class="dropdown">
+                <summary class="btn btn-ghost font-semibold text-lg text-[#E178C5]">
+                    <p>Payment</p>
+                    <img src="../images/background/dropdownBtn.svg" class="w-4 p-0 md:w-3" />
+                </summary>
+                <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                    <li>
+                        <a href="/src/Public/nasabah/wajib.php" class="text-[#E178C5] font-semibold">
+                            <i class="ph ph-wallet text-xl"></i>
+                            Tabungan Wajib
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/src/Public/nasabah/sukarela.php" class="text-[#FFB38E] font-semibold">
+                            <i class="ph ph-hand-coins text-xl"></i>
+                            Tabungan Sukarela
+                        </a>
+                    </li>
+                </ul>
+            </details>
+
+        </div>
+
+        <details class="dropdown dropdown-end">
+            <summary class="btn btn-link no-underline hover:no-underline">
+                <img src="../images/profile/dummyProfile.svg" class="w-14 md:w-11" />
+                <div class="hidden md:flex flex-col items-start">
+                    <p class="font-semibold text-[#E178C5]">
+                        <?= $user["nama"] ?>
+                    </p>
+                    <p class="font-light text-[#E178C5]/50">Nasabah</p>
+                </div>
+            </summary>
+            <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                <div class="flex md:hidden flex-col p-4">
+                    <p class="font-semibold text-[#E178C5]">
+                        <?= $user["nama"] ?>
+                    </p>
+                    <p class="font-light text-[#E178C5]/50">Nasabah</p>
+
+                    <hr class="my-2 border-[#E178C5] w-full" />
+                </div>
+
+                <li>
+                    <div>
+                        <p class="ph ph-pencil-simple-line text-xl"></p>
+                        <a href="./profile.php">Edit profile</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="text-[#FF8E8F]">
+                        <p class="ph ph-sign-out text-xl"></p>
+                        <a href="../logout.php">Logout</a>
+                    </div>
+                </li>
+            </ul>
+        </details>
+    </div>
+    <!-- content -->
+    <div class="flex flex-1 h-full my-4 justify-center items-center">
+        <div class="flex flex-col bg-black w-64 h-auto p-5">
+            <div class="flex justify-around">
+                <span class="text-2xl font-bold text-[#FF8E8F] m-5">
+                    My Account
+                </span>
+
+            </div>
+            <div class="flex">
+                <img src="../images/profile/dummyProfile.svg" class="w-14 md:w-11 mr-[5rem]" />
+                <div class="md:flex flex-col items-start">
+                    <p class="font-semibold text-[#E178C5]">
+                        <?= $user["nama"] ?>
+                    </p>
+                    <p class="font-light text-[#E178C5]/50">Nasabah</p>
+                </div>
+            </div>
+            <hr class="my-2 border-[#E178C5] w-full" />
+        </div>
+    </div>
+
+
+    <footer class="footer footer-center items-center justify-center text-white font-semibold bg-[url('../images/background/bottom.svg')] fixed inset-x-0 bottom-0">
+        <p class="text-center z-10 p-4">©2024 UnityBook. All rights reserved.</p>
+    </footer>
+
+</body>
+
+</html>
