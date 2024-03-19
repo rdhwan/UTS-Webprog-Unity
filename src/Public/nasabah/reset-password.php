35cf052e5eb5f78c757cd7144a304250abc06940
AlbertTech23 <alberttirtokusumo@gmail.com>
2024-03-19 15:37:57 +0700

    feat: reset password feature done. NOTE: need checking, because my php goes sick again, tailwind doesn't work and can't see changes

--- src/Public/nasabah/reset-password.php
+++ src/Public/nasabah/reset-password.php
@@ -1,8 +1,27 @@
 <?php
-// @albert
-
+require_once __DIR__ . "/../../bootstrap.php";
 require_once __DIR__ . "/../../Middleware/checkNasabah.php";
 
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $newPassword = $_POST["password"];
+    $confirmPassword = $_POST["confirm_password"];
+
+    if ($newPassword === $confirmPassword) {
+        $userId = $_SESSION["user_id"];
+        $user = User::find($userId);
+        $hashedPassword = password_hash($newPassword, PASSWORD_BCRYPT);
+        $user->password = $hashedPassword;
+        $user->save();
+        $_SESSION["error"] = "Passwords changed successfully!";
+        header("Location: reset-password.php");
+        exit;
+    } else {
+        $_SESSION["error"] = "Passwords don't match or empty.";
+        header("Location: reset-password.php");
+        exit;
+    }
+}
 ?>
 
 
@@ -28,18 +47,15 @@ require_once __DIR__ . "/../../Middleware/checkNasabah.php";
             </summary>
             <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
                 <li>
-                    <a href="./history.php"
-                        class="btn btn-ghost flex items-center justify-start font-semibold text-lg text-[#E178C5]">History
+                    <a href="./history.php" class="btn btn-ghost flex items-center justify-start font-semibold text-lg text-[#E178C5]">History
                     </a>
                 </li>
                 <li>
                     <details class="dropdown">
-                        <summary
-                            class="btn btn-ghost flex items-center justify-start font-semibold text-lg text-[#E178C5]">
+                        <summary class="btn btn-ghost flex items-center justify-start font-semibold text-lg text-[#E178C5]">
                             <p>Payment</p>
                         </summary>
-                        <ul tabindex="0"
-                            class="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                        <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
                             <li>
                                 <a href="/src/Public/nasabah/wajib.php" class="text-[#E178C5] font-semibold">
                                     <i class="ph ph-wallet text-xl"></i>
@@ -130,8 +146,7 @@ require_once __DIR__ . "/../../Middleware/checkNasabah.php";
 
     <!-- content -->
     <div class="flex flex-1 h-full my-4 justify-center items-center">
-        <div
-            class="mb-[3rem] md:mb-0 flex flex-col relative bg-gradient-to-r from-[#E178C5] to-[#FFB38E] p-5 w-[30rem] h-auto rounded-2xl shadow-lg">
+        <div class="mb-[3rem] md:mb-0 flex flex-col relative bg-gradient-to-r from-[#E178C5] to-[#FFB38E] p-5 w-[30rem] h-auto rounded-2xl shadow-lg">
             <div class="bg-[#f6f6f6] rounded-xl md:p-[2rem] p-[1rem]">
                 <div class="flex justify-between">
                     <span class="md:text-3xl text-xl font-bold text-[#FF8E8F]">
@@ -142,33 +157,41 @@ require_once __DIR__ . "/../../Middleware/checkNasabah.php";
                         <p class="text-sm text-[#FF8E8F] font-bold hidden sm:block">ESC</p>
                     </a>
                 </div>
-                <div class="mb-5"><span class="font-light text-[#FF8E8F]">New Password</span>
-                    <label class="my-5 flex flex-row bg-transparent rounded-none border-b-2 pb-2 w-full">
-                        <i class="ph ph-lock opacity-35 text-2xl me-3"></i>
-                        <input type="password" required name="password" class=" w-full bg-transparent"
-                            placeholder="Type new password" />
-                    </label>
-                </div>
-                <div class="my-5"><span class="font-light text-[#FF8E8F]">Confirm Password</span>
-                    <label class="my-5 flex flex-row bg-transparent rounded-none border-b-2 pb-2 w-full">
-                        <i class="ph ph-lock opacity-35 text-2xl me-3"></i>
-                        <input type="password" required name="password" class="w-full bg-transparent"
-                            placeholder="Confirm new password" />
-                    </label>
-                </div>
-                <a href="reset-password.php">
-                    <button
-                        class="mt-5 flex justify-center items-center w-[10rem] h-[2rem] p-3 bg-[#FF8E8F] rounded-[0.5rem] text-[#FFFDCB] font-bold text-sm shadow-lg ">
-                        Change
-                        Password</button>
-                </a>
+                <form action="reset-password.php" method="POST" class="mb-5">
+                    <div class="my-5">
+                        <span class="font-light text-[#FF8E8F]">New Password</span>
+                        <label class="my-5 flex flex-row bg-transparent rounded-none border-b-2 pb-2 w-full">
+                            <i class="ph ph-lock opacity-35 text-2xl me-3"></i>
+                            <input type="password" required name="password" class="w-full bg-transparent" placeholder="Type new password" />
+                        </label>
+                    </div>
+                    <div class="my-5">
+                        <span class="font-light text-[#FF8E8F]">Confirm Password</span>
+                        <label class="my-5 flex flex-row bg-transparent rounded-none border-b-2 pb-2 w-full">
+                            <i class="ph ph-lock opacity-35 text-2xl me-3"></i>
+                            <input type="password" required name="confirm_password" class="w-full bg-transparent" placeholder="Confirm new password" />
+                        </label>
+                    </div>
+                    <?php if ($error) : ?>
+                        <p class="text-red-400">
+                            <?= $error ?>
+                        </p>
+                    <?php endif; ?>
+                    <?php if ($success) : ?>
+                        <p class="text-green-400">
+                            <?= $success ?>
+                        </p>
+                    <?php endif; ?>
+                    <button type="submit" class="mt-5 flex justify-center items-center w-[10rem] h-[2rem] p-3 bg-[#FF8E8F] rounded-[0.5rem] text-[#FFFDCB] font-bold text-sm shadow-lg">
+                        Change Password
+                    </button>
+                </form>
             </div>
         </div>
     </div>
 
 
-    <footer
-        class="footer footer-center items-center justify-center text-white font-semibold bg-[url('../images/background/bottom.svg')] fixed inset-x-0 bottom-0">
+    <footer class="footer footer-center items-center justify-center text-white font-semibold bg-[url('../images/background/bottom.svg')] fixed inset-x-0 bottom-0">
         <p class="text-center z-10 p-4">©2024 UnityBook. All rights reserved.</p>
     </footer>
 
